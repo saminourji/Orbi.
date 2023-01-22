4b0fc4362d25422ae12803905063affe4396192a
saminourji <saminourji23@gmail.com>
2023-01-21 19:57:42 -0800

    Added details for rings + started info boxes

--- extension/src/App.js
+++ extension/src/App.js
@@ -2,23 +2,30 @@ import './App.css';
 import logo from "./assets/Orbi..svg";
 
 function App() {
+  let perct1 = 45;
+  let color1 = "#D0DFDE";
+  let perct2 = 63;
+  let color2 = "#A8AEA7";
+  let perct3 = 85;
+  let color3 = "#67675E";
   return (
 <body>
-  <header class = "logo">
+  <header className = "logo">
     <img src = {logo} alt = "orbi logo"/>
   </header>
     <div className="rings">
-      <Ring radius="4" width="3.5" color="#D0DFDE" perct="45"/>
-      <Ring radius="8" width="3.5" color="#A8AEA7" perct="63"/>
-      <Ring radius="12" width="3.5" color="#67675E" perct="85"/>
+      <Ring radius="4" width="3.5" color={color1} perct={perct1}/>
+      <Ring radius="8" width="3.5" color={color2} perct={perct2}/>
+      <Ring radius="12" width="3.5" color={color3} perct={perct3}/>
+    </div>
+    <div className="rects">
+      <Rect hours="2.5" title="Work" color={color1}/>
+      <Rect hours="2.5" title="Work" color={color1}/>
+      <Rect hours="2.5" title="Work" color={color1}/>
     </div>
     {/* This is the <span class="special-text">TML</span> body of the addon. */}
 </body>
   );
-}
-
-function Test() {
-  return (<h1>Hello m8</h1>)
 }
 
 function Ring(props) {
@@ -28,6 +35,16 @@ function Ring(props) {
   let color = props.color;
   return (
   <svg viewBox="0 0 36 36" className="circular-chart">
+    
+    <path 
+      d={"M18 " + (18-(radius+width)) +
+        " a " + String(radius) + " " + String(radius) + " 0 0 1 0 " + String(radius*2) +
+        " a " + String(radius) +  " " + String(radius) + " 0 0 1 0 " + String(-radius*2)}
+      fill="none"
+      stroke={color + "46"}
+      stroke-width={String(width)}
+      stroke-dasharray={String(3.1415*radius*2) + ", " + String(3.1415*radius*2)}
+    />
     <path className="circle"
       d={"M18 " + (18-(radius+width)) +
         " a " + String(radius) + " " + String(radius) + " 0 0 1 0 " + String(radius*2) +
@@ -38,7 +55,15 @@ function Ring(props) {
       stroke-dasharray={String(3.1415*radius*2*perct/100) + ", " + String(3.1415*radius*2)}
     />
   </svg>
+  
+  
   )
+}
+function Rect(props) {
+  let hours = props.hours;
+  let title = props.title;
+  let color = props.color;
+  return (<div className="box"> "hELL"</div>)
 }
 
 export default App;
